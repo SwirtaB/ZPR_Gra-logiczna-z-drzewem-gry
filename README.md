54a3b60fffb30e7a74cee444f5f6c15c2b256b37
Maciej Wojno <git@mwojno.me>
2020-11-15 12:47:43 +0100

    Przykladowy Qt5 i Boost

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ZPR_Gra-logiczna-z-drzewem-gry
+
+## Wymagania
+- Dynamiczna biblioteka Qt5 wykrywalna przez cmake
+- Biblioteka Boost wykrywalna przez cmake
+
+## Budowanie i uruchomienie
+```
+mkdir cmake-build
+cd cmake-build
+cmake ..
+cmake --build .
+./Debug/hello.exe
+```
---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <string>
+
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
+
+#include <boost/shared_ptr.hpp>
+//#include <boost/python.hpp>
+
+
+int main(int argc, char *argv[])
+{
+    //Py_Initialize();
+
+    boost::shared_ptr<QString> a(new QString("iksde"));
+
+    QApplication app(argc, argv);
+    QLabel *label = new QLabel(*a);
+    label->setFixedSize(200, 200);
+    label->setAlignment(Qt::AlignmentFlag::AlignCenter);
+    label->show();
+    int code = app.exec();
+    return code;
+}
