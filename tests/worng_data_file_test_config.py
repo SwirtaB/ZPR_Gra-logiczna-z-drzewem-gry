aeffe5473399f3f7eac80b9b8196fac7cecf29b1
SwirtaB <git@swirta.pl>
2021-01-10 18:28:13 +0100

    Python scripts for testing.

---
+++ tests/worng_data_file_test_config.py
@@ -0,0 +1,16 @@
+""" przykladowy config
+cross_is_bot = True / False
+circle_is_bot = True / False
+start = "circle" / "cross"
+bot_tactic = "heuristic" / "game tree"
+ui_size = "small" / "medium" / "large" / "resizable"
+save_path = "path/to/save"
+
+"""
+cross_is_bot = False
+circle_is_bot = True
+start = "cross"
+bot_tactic = "game_tree"
+ui_size = "small"
+depth = 10
+save_path = "save.txt"
